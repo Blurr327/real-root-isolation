71901382e6c9da6d6b5d24df8e408f53ed770e2e
Mohamed BEN EL MOSTAPHA <benelmed36@gmail.com>
2026-05-23 13:50:09 +0200

    add flint taylor shift parallelism

--- src/parallel_rri_algo.c
+++ src/parallel_rri_algo.c
@@ -26,8 +26,8 @@ void par_subdiv_algo_ext(fmpz_poly_t in_poly, fmpq_vec_t *sol, fmpq_t start,
   fmpz_poly_reverse(tmp_poly, in_poly, fmpz_poly_degree(in_poly) + 1);
 
   // x -> x + 1
-  // fmpz_poly_taylor_shift_split_parallel(tmp_poly, tmp_poly, tmp);
-  fmpz_poly_taylor_shift(tmp_poly, tmp_poly, tmp);
+  fmpz_poly_taylor_shift_multi_mod(tmp_poly, tmp_poly, tmp);
+  // fmpz_poly_taylor_shift_multi_mod(tmp_poly, tmp_poly, tmp);
 
   int c = count_sign_variations(tmp_poly);
 
@@ -60,8 +60,8 @@ void par_subdiv_algo_ext(fmpz_poly_t in_poly, fmpq_vec_t *sol, fmpq_t start,
 
   // RIGHT: x = (y+1)/2
   shift_in_proportions_by_k(right_poly, in_poly, -1);
-  // fmpz_poly_taylor_shift_split_parallel(right_poly, right_poly, tmp);
-  fmpz_poly_taylor_shift(right_poly, right_poly, tmp);
+  fmpz_poly_taylor_shift_multi_mod(right_poly, right_poly, tmp);
+  // fmpz_poly_taylor_shift_multi_mod(right_poly, right_poly, tmp);
 
   // task creation
 #pragma omp task shared(sol) if (depth < MAX_PARALLELIZATION_DEPTH)
@@ -83,9 +83,12 @@ cleanup:
 }
 
 void par_subdiv_algo(fmpz_poly_t in_poly, fmpq_vec_t *sol) {
+  int flint_num_threads = 4;
   fmpq_t bound;
   fmpq_t start, end;
   fmpz_poly_t tmp_poly;
+
+  flint_set_num_threads(flint_num_threads);
 
   fmpz_poly_init(og_poly);
 
